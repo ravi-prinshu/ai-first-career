782c9d8bd31dc27a17bab37acb53d078346aaaf1
Ravi Prinshu <RAVIPRINSHU@GMAIL.COM>
2026-08-16 12:44:51 +0530

    Complete Week 2 Python practice

--- practice.py
+++ practice.py
@@ -1,1 +1,5 @@
-print("I am learning to build with AI.")
+name = input("What is your name? ")
+print("Hello", name, "welcome to Python!")
+age = int(input("What is your age? "))
+next_year_age = age + 1
+print("Next year you will be", next_year_age)
